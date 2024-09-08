3c4874ccfd60fbe7118e9002e1776b8cf1df39a6
Andsu <antipiskun@gmail.com>
2024-09-08 17:53:22 +0300

    skinCareApp MainPage with buttons for choosing picture

--- skinCareApp/App.tsx
+++ skinCareApp/App.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import MainPage from './pages/MainPage'
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>App</Text>
+      <MainPage/>
     </View>
   )
 }
 
 export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Заполняем весь экран
+  },
+});
---
+++ skinCareApp/pages/MainPage.tsx
@@ -0,0 +1,72 @@
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
+import React, { useState } from 'react'
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker'
+
+export default function MainPage() {
+
+    const [photo, setPhoto] = useState<{ uri: string } | null>(null); // Определяем тип для состояния
+
+    const takePhoto = () => {
+      launchCamera(
+        {
+          mediaType: 'photo',
+          saveToPhotos: true,
+        },
+        (response) => {
+          if (response.didCancel) {
+            console.log('Пользователь отменил выбор фото');
+          } else if (response.errorCode) {
+            console.log('Ошибка ImagePicker: ', response.errorMessage);
+          } else if (response.assets && response.assets.length > 0 && response.assets[0].uri) {
+            const source = { uri: response.assets[0].uri }; // Проверяем наличие uri
+            setPhoto(source); // Сохраняем фото
+          }
+        }
+      );
+    };
+
+  const chooseFromLibrary = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo', // Работаем с фото
+      },
+      (response) => {
+        if (response.didCancel) {
+          console.log('Пользователь отменил выбор фото');
+        } else if (response.errorCode) {
+          console.log('Ошибка ImagePicker: ', response.errorMessage);
+        } else if (response.assets && response.assets.length > 0 && response.assets[0].uri) {
+          const source = { uri: response.assets[0].uri }; // Проверяем наличие uri
+          setPhoto(source); // Сохраняем изображение в состояние
+        }
+      }
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Главная страница</Text>
+      <Button title="Сделать фото" onPress={takePhoto} /> 
+      <Button title="Выбрать из галереи" onPress={chooseFromLibrary} /> 
+      {photo && <Image source={photo} style={styles.image} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f5f5f5',
+    },
+    title: {
+      fontSize: 24,
+      marginBottom: 20,
+    },
+    image: {
+      width: 200,
+      height: 200,
+      marginTop: 20,
+    },
+});
