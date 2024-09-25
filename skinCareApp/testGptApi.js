b91171b24242f3b218425865680d1348435c06ae
Andsu <antipiskun@gmail.com>
2024-09-25 20:41:08 +0300

    testGptApi is working with local image

--- skinCareApp/testGptApi.js
+++ skinCareApp/testGptApi.js
@@ -1,44 +1,48 @@
+const fs = require('fs').promises;
+const OpenAI = require('openai');
 const dotenv = require('dotenv');
-
 
 dotenv.config();
 
-const OpenAI = require('openai');
+async function askAboutImage(imageFilePath) {
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-
-
-async function testGPT() {
-  console.log(process.env.OPENAI_API_KEY);
+  // Convert the image to WebP format using sharp
+  console.log("1");
 
   try {
+    const imageBuffer = await fs.readFile(imageFilePath);
+
+    const base64Image = imageBuffer.toString('base64');
+    const encodedImage = `data:image/jpeg;base64,{${base64Image}}`;
+
     const response = await openai.chat.completions.create({
-      model: "gpt-4o",
-      messages: [
-        {
-          "role": "user",
-          "content": [
-            {"type": "text", "text": "What’s in this image?"},
+        model: "gpt-4o",
+        messages: [
             {
-              "type": "image_url",
-              "image_url": {
-                "url": "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-              },
+                role: "user",
+                content: [
+                    { type: "text", text: "what is in image?" },
+                    { type: "image_url", image_url: { "url": encodedImage } },
+                ],
             },
-          ],
-        }
-      ]
+        ],
+        max_tokens: 1024,
     });
 
-    console.log('Response:', response.choices[0].message.content);
+    console.log(response.choices[0].message);
+
+    const responseMessage = response.choices[0].message;
+    return responseMessage;
+
   } catch (error) {
-    console.error('Error with GPT API:', error);
+    console.error('Error reading image:', error);
   }
+  
+  
 }
 
-testGPT();
+// Example usage with one image
+(async () => {
+  await askAboutImage('./test.png');
+})();
