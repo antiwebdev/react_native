577c771a8fa4d9dfda1945c2804b805698a9fdf5
Andsu <antipiskun@gmail.com>
2024-10-27 21:31:33 +0200

    fixed onPress callback type issue

--- PasswordGenerator/App.tsx
+++ PasswordGenerator/App.tsx
@@ -148,7 +148,7 @@ export default function App() {
                   <TouchableOpacity
                     disabled={!isValid}
                     style={styles.primaryBtn}
-                    onPress={handleSubmit}
+                    onPress={() => handleSubmit()}
                   >
                     <Text style={styles.primaryBtnTxt}>Generate Password</Text>
                   </TouchableOpacity>
