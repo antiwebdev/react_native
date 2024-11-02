6f2539f46ac954012f806fb651c776f1f82bd3fb
Andsu <antipiskun@gmail.com>
2024-11-02 15:40:46 +0200

    haptic-feedback package linking

--- Project04/src/App.tsx
+++ Project04/src/App.tsx
@@ -11,6 +11,8 @@ import {
   Pressable
 } from 'react-native';
 
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
+
 import DiceOne from '../assets/One.png';
 import DiceTwo from '../assets/Two.png';
 import DiceThree from '../assets/Three.png';
@@ -21,6 +23,11 @@ import DiceSix from '../assets/Six.png';
 type DiceProps = PropsWithChildren<{
   imageUrl: ImageSourcePropType
 }>
+
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
 
 const Dice = ({imageUrl}: DiceProps):JSX.Element => {
   return (
@@ -62,6 +69,7 @@ function App(): React.JSX.Element {
         setDiceImg(DiceOne);
         break;
     }
+    ReactNativeHapticFeedback.trigger("impactLight", options);
     console.log(randomNum)
   }
   
