7c5c526f932595a14da8e75c1e864387912633e6
Andsu <antipiskun@gmail.com>
2024-11-03 15:33:49 +0200

    currency app added as folder

---
+++ EuroToCurrencyApp/src/constants.ts
@@ -0,0 +1,56 @@
+export const currencyByEuro: Currency[] = [
+    {
+      name: 'DOLLAR',
+      value: 1.09, // 1 Euro ≈ 1.09 USD
+      flag: '🇺🇸',
+      symbol: '$',
+    },
+    {
+      name: 'RUPEE',
+      value: 88.87, // 1 Euro ≈ 88.87 INR
+      flag: '🇮🇳',
+      symbol: '₹',
+    },
+    {
+      name: 'POUND',
+      value: 0.87, // 1 Euro ≈ 0.87 GBP
+      flag: '🇬🇧',
+      symbol: '£',
+    },
+    {
+      name: 'RUBEL',
+      value: 94.96, // 1 Euro ≈ 94.96 RUB
+      flag: '🇷🇺',
+      symbol: '₽',
+    },
+    {
+      name: 'AUS DOLLAR',
+      value: 1.63, // 1 Euro ≈ 1.63 AUD
+      flag: '🇦🇺',
+      symbol: 'A$',
+    },
+    {
+      name: 'CAN DOLLAR',
+      value: 1.44, // 1 Euro ≈ 1.44 CAD
+      flag: '🇨🇦',
+      symbol: 'C$',
+    },
+    {
+      name: 'YEN',
+      value: 158.29, // 1 Euro ≈ 158.29 JPY
+      flag: '🇯🇵',
+      symbol: '¥',
+    },
+    {
+      name: 'DINAR',
+      value: 0.31, // 1 Euro ≈ 0.31 KWD
+      flag: '🇰🇼',
+      symbol: 'KD',
+    },
+    {
+      name: 'BITCOIN',
+      value: 0.000029, // 1 Euro ≈ 0.000029 BTC
+      flag: '🎰',
+      symbol: '₿',
+    },
+  ];
