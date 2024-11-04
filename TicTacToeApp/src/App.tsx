1d45e696a428b16eb4528d9b101363a91b8f653b
Andsu <antipiskun@gmail.com>
2024-11-04 20:03:08 +0200

    TicTacToeApp created, components created, packages imported, waiting for wining logic

---
+++ TicTacToeApp/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
