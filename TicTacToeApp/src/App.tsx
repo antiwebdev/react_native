59f3c72dc46f7718fff0678f7b0caa13bb9a98ef
Andsu <antipiskun@gmail.com>
2024-11-05 22:46:07 +0200

    Game is working, need to fix O icon, and add Text component

--- TicTacToeApp/src/App.tsx
+++ TicTacToeApp/src/App.tsx
@@ -4,7 +4,10 @@ import Icons from './components/Icons';
 
 
 import {
+  FlatList,
+  Pressable,
   SafeAreaView,
+  StatusBar,
   StyleSheet,
   Text,
   View,
@@ -102,15 +105,117 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView>
+      <StatusBar/>
+      // Winner/Turn Section
+      {gameWinner ? (
+        <View style={[styles.playerInfo, styles.winnerInfo]}>
+          <Text style={styles.winnerTxt}>{gameWinner}</Text>
+        </View>
+      ) : (
+        <View style={[styles.playerInfo, isCross ? (styles.playerX) : (styles.playerO)]}>
+          <Text style={styles.gameTurnTxt}>Player {isCross ? 'X' : 'O'}</Text>
+        </View>
+      )}
+      //Game Section
+      <FlatList
+        numColumns={3}
+        data={gameState}
+        renderItem={({item, index}) => (
+          <Pressable
+            key={index}
+            style={styles.card}
+            onPress={() => onChangeItem(index)}
+          >
+            <Icons name={item}/>
+          </Pressable>
+        )}
+        style={styles.grid}
+      />
+      //Restar Section
       <View>
-
+        <Pressable
+          style={styles.gameBtn}
+          onPress={reloadGame}
+        >
+          <Text style={styles.gameBtnText}>
+            {gameWinner ? "Start new Game" : "Reload the game"}
+          </Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+   playerInfo: {
+    height: 56,
 
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginVertical: 12,
+    marginHorizontal: 14,
+
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
+  },
+  gameTurnTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  playerX: {
+    backgroundColor: '#38CC77',
+  },
+  playerO: {
+    backgroundColor: '#F7CD2E',
+  },
+  grid: {
+    margin: 12,
+  },
+  card: {
+    height: 100,
+    width: '33.33%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  winnerInfo: {
+    borderRadius: 8,
+    backgroundColor: '#38CC77',
+
+    shadowOpacity: 0.1,
+  },
+  winnerTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+  },
+  gameBtn: {
+    alignItems: 'center',
+
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 36,
+    backgroundColor: '#8D3DAF',
+  },
+  gameBtnText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500',
+  },
 });
 
 export default App;
