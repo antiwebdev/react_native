47e209bfa9f1f37e5ce204f13bc10767b0175593
Andsu <antipiskun@gmail.com>
2024-11-09 15:40:29 +0200

    Added state for player setup

--- MusicPlayerApp/src/App.tsx
+++ MusicPlayerApp/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  ActivityIndicator,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+
+import { setupPlayer, addTrack } from '../musicPlayerServices';
 
 
 function App(): React.JSX.Element {
 
+  const [isPlayerReady, setIsPlayerReady] = useState(false)
+
+  async function setup() {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPlayerReady(isSetup)
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+  if (!isPlayerReady) {
+    return(
+      <SafeAreaView>
+        <ActivityIndicator/>
+      </SafeAreaView>
+    )
+  }
+  
   return (
-    <>
-    </>
+    <SafeAreaView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1
+  }
 });
 
 export default App;
