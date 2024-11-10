a08b976846e35f8c25f3ea4ac87ecb858d8cb9cf
Andsu <antipiskun@gmail.com>
2024-11-10 19:41:49 +0200

    Refactor: Add MusicPlayer screen and handle track change event (nextTrack issue pending)

--- MusicPlayerApp/src/components/SongInfo.tsx
+++ MusicPlayerApp/src/components/SongInfo.tsx
@@ -22,27 +22,26 @@ const SongInfo = ({track}: SongInfoProops) => {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: '90%',
-        marginTop: 18,
-    
-        flexDirection: 'row',
-        alignItems: 'baseline',
-        justifyContent: 'center',
-      },
-      name: {
-        marginBottom: 8,
-        textAlign: 'center',
-    
-        color: '#fff',
-        fontSize: 24,
-        fontWeight: '800',
-      },
-      artist: {
-        color: '#d9d9d9',
-        textAlign: 'center',
-      },
-    });
-})
+  container: {
+    width: '90%',
+    marginTop: 18,
+
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    justifyContent: 'center',
+  },
+  name: {
+    marginBottom: 8,
+    textAlign: 'center',
+
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: '800',
+  },
+  artist: {
+    color: '#d9d9d9',
+    textAlign: 'center',
+  },
+});
 
 export default SongInfo
---
+++ MusicPlayerApp/src/screens/MusicPlayer.tsx
@@ -0,0 +1,75 @@
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
+import React, {useState} from 'react'
+import TrackPlayer, {Event, Track, useTrackPlayerEvents} from 'react-native-track-player';
+import { playListData } from '../constants';
+import SongInfo from '../components/SongInfo';
+import SongSlider from '../components/SongSlider';
+import ControlCenter from '../components/ControlCenter';
+
+
+
+const {width} = Dimensions.get('window');
+
+const MusicPlayer = () => {
+
+    const [track, setTrack] = useState<Track | null>()
+
+    useTrackPlayerEvents([Event.PlaybackActiveTrackChanged], async event => {
+        switch (event.type) {
+            case Event.PlaybackActiveTrackChanged:
+                const playingTrack = await TrackPlayer.getActiveTrack(event.nextTrack);
+                setTrack(playingTrack)
+                break;
+        
+            default:
+                break;
+        }
+    }) 
+
+    const renderArtWork = () => {
+        return(
+            <View style={styles.listArtWrapper}>
+                <View style={styles.albumContainer}>
+                    {track?.artwork && (
+                        <Image
+                        style={styles.albumArtImg}
+                        source={{
+                            uri: track?.album?.toString()
+                        }}
+                        />
+                    )}
+                </View>
+            </View>
+        )
+    }
+
+  return (
+    <View>
+      <Text>MusicPlayer</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#001d23',
+    },
+    listArtWrapper: {
+        width: width,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    albumContainer: {
+        width: 300,
+        height: 300,
+    },
+    albumArtImg: {
+        height: '100%',
+        borderRadius: 4,
+    },
+})
+
+export default MusicPlayer
